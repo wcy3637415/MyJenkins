469b9eeb015ddef9998f8e12af96e0009eb42e46
Wcy <wcy3637415@126.com>
2018-12-29 17:12:17 +0800

    master

--- app/src/main/java/com/nantian/myjenkins/MainActivity.java
+++ app/src/main/java/com/nantian/myjenkins/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "元旦快乐11111", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
